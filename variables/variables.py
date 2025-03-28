567dd7eec94b3d892481964a127f0c9ac44238ef
carme-hp <homspons@gmail.com>
2025-03-28 13:07:20 +0100

    Change sending direction for traction

--- variables/variables.py
+++ variables/variables.py
@@ -3,7 +3,7 @@ precice_file = "../precice-config.xml"
 # Timestep
 #################################################################
 
-dt_3D = 0.1             # time step of 3D mechanics
+dt_3D = 0.01             # time step of 3D mechanics
 dt_splitting = 2e-3     # time step of strang splitting
 dt_1D = 2e-3            # time step of 1D fiber diffusion
 dt_0D = 1e-3            # time step of 0D cellml problem
